6b60cb47916922ce1a3455566a075e1a23e5051a
nehubuser <oldfaggio@gmail.com>
2025-06-09 16:49:36 +0545

    Update check.js
    SRC placeholder.

--- check.js
+++ check.js
@@ -2,7 +2,7 @@
 		/* Check status */
 		var c=document.getElementById('doTest');
 		var i=document.createElement('img');
-		i.src='https://e6.pages.dev/img/x.png?_='+Date.now();
+		i.src=' ... x.png?_='+Date.now();
 		i.onload=function(){c.innerHTML = "<span class='text-danger'>Unable to check!</span>";};
 		i.onerror=function(){c.innerHTML = "<span class='text-success'>Enabled</span>";};
 		})();
